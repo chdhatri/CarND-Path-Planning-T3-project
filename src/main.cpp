131fad6b8993afaf7ee12aa4e11c1dd5abf96b7b
chdhatri <dhatric@sarma-2.local>
2018-07-05 22:45:53 +0530

    Changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -253,13 +253,27 @@ int main() {
                     
                     
                     bool too_close = false;
+                    bool left_is_free = true;
+                    bool right_is_free = true;
                     
+                    double space_on_left = 10000.0;
+                    double space_on_right = 10000.0;
+                    
+                    
+                    // check which lanes are not free depending on the ego vehichle's lane
+                    if (lane==0)
+                    {
+                        left_is_free = false;
+                    }
+                    if (lane==2)
+                    {
+                        right_is_free = false;
+                    }
                     
                     //find ref_v to use
                     for(int i =0; i < sensor_fusion.size(); i++)
                     {
-                        float d = sensor_fusion[i][6];
-                        if(d < (2 + 4*lane + 2) && d > (2 + 4* lane -2)) {
+                            float d = sensor_fusion[i][6];
                             double vx = sensor_fusion[i][3];
                             double vy = sensor_fusion[i][4];
                             double check_speed = sqrt(vx*vx + vy*vy);
@@ -267,17 +281,92 @@ int main() {
                             
                             //Estimate car s position after executing previous trajectory
                             check_car_s += ((double)prev_size * .02 * check_speed);
+                        
+                            int lane_obsvd = fabs(d/4);
+
+                            
+                            double space = check_car_s - end_path_s;
+                            
+                            double car_dist = check_car_s - car_s;
                             
                             //check s values greater than mine and s gap
-                            if((check_car_s > car_s) && ((check_car_s - car_s) < 30)) {
-                                too_close = true;
-                                if(lane > 0) {
-                                    lane = 0;
+                            if ((car_dist > 0) && (car_dist < 30))
+                            {
+                                if (lane_obsvd == (lane-1)){ // if an observed car is on the left side
+                                    if (space < space_on_left) {space_on_left = space;}
+                                    left_is_free = false;
                                 }
+                                if (lane_obsvd == (lane+1)){ // if an observed car is on the right side
+                                    if (space < space_on_right) {space_on_right = space;}
+                                    right_is_free = false;
+                                }
+                            }
+                            // if car is at the back, within 20m
+                            else if ((car_dist < 0) && (car_dist > -20))
+                            {
+                                if (lane_obsvd == (lane-1)){ // if an observed car is on the left side
+                                    left_is_free = false;
+                                }
+                                if (lane_obsvd == (lane+1)){ // if an observed car is on the right side
+                                    right_is_free = false;
+                                }
+                            }
+                        // if observed car is in my lane:
+                        if ((d<2+4*lane+2) && d > (2+4*lane-2))
+                        {
+                            // if observed car is too close
+                            if ((check_car_s > car_s) && ((check_car_s-car_s)<30))
+                            {
+                                too_close = true;
                                 
+                                if (lane_obsvd == (lane-1)){ // if an observed car is on the left side
+                                    // if (space < space_on_left) {space_on_left = space;}
+                                    left_is_free = false;
+                                }
+                                if (lane_obsvd == (lane+1)){ // if an observed car is on the right side
+                                    // if (space < space_on_right) {space_on_right = space;}
+                                    right_is_free = false;
+                                }
                             }
                         }
                     }
+                    // if car is too close
+                    if (too_close)
+                    {
+                        // cout << "\nleft_side: " << left_is_free << ", " << space_on_left
+                        //     << " right_side: " << right_is_free << ", " << space_on_right
+                        //     << endl;
+                        if ((lane==0) && right_is_free)
+                        {
+                            lane = 1;
+                        }
+                        else if (lane==1)
+                        {
+                            if (left_is_free && right_is_free)
+                            {
+                                if (space_on_right > space_on_left)
+                                {
+                                    lane += 1;
+                                } else {
+                                    lane -= 1;
+                                }
+                            } else if (left_is_free) {
+                                lane -= 1;
+                            } else if(right_is_free) {
+                                lane += 1;
+                            }
+                        }
+                        else if ((lane==2) && left_is_free)
+                        {
+                            lane = 1;
+                        }
+                    }
+                    
+                    if (lane>2)
+                    {
+                        lane = 2;
+                    }
+
                     
                     if(too_close) {
                         ref_vel -= .224;
